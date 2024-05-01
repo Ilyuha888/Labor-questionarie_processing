a1cce93a3b8539d039a46a7d98b032bf4afd77fe
Ilyuha888 <nartov.nartov@gmail.com>
2024-05-01 11:33:20 +0300

    Поправил обработку 2й волны сбора данных

--- 2nd_survey/Anal_1.R
+++ 2nd_survey/Anal_1.R
@@ -36,12 +36,13 @@ View(kurtosis)
 
 
 anket %>% 
-  select(-a_cosv15) -> anket
+  select(-a_cosv15, -a3) -> anket
 
 #Пересчитаем суммы
 
 anket %>% 
   mutate(
+    a_sum_1 = rowSums(across(c(a1:a13, a_cosv1:a_cosv5))),
     a_sum_4 = rowSums(across(c(a43:a56, a_cosv16:a_cosv19)))) -> anket
 
 #МОДЕЛЬ 1 -----
@@ -64,11 +65,9 @@ anket %>%
 #где показатель ниже 0.7
 
 anket %>% 
-  select(-a12, -a48, -a18, -a33, -a_cosv6, -a36, -a54, -a5, -a26, -a34, -a21, 
-         -a40, -a49, -a22, -a10, -a17, -a23, -a31, -a_cosv1, -a3, -a45, 
-         -a35, -a_cosv5, -a53, -a_cosv19) -> anket
+  select(-a12, -a48, -a18, -a33, -a_cosv6, -a36, -a54, -a40) -> anket
 
-anket %>% select(a1:a_cosv18) %>% colnames() -> vars_left
+anket %>% select(a1:a_cosv19) %>% colnames() -> vars_left
 
 vars_left
 
@@ -76,15 +75,15 @@ vars_left
 
 anket %>% 
   mutate(
-    a_sum_1 = rowSums(across(c(a1:a13, a_cosv2:a_cosv4))),
+    a_sum_1 = rowSums(across(c(a1:a13, a_cosv1:a_cosv5))),
     a_sum_2 = rowSums(across(c(a14:a27, a_cosv7:a_cosv10))),
     a_sum_3 = rowSums(across(c(a28:a42, a_cosv11:a_cosv14))),
-    a_sum_4 = rowSums(across(c(a43:a56, a_cosv16:a_cosv18)))) -> anket
+    a_sum_4 = rowSums(across(c(a43:a56, a_cosv16:a_cosv19)))) -> anket
 
 #Альфа Кронбаха
 
 anket %>% 
-  select(a1:a39) %>%
+  select(a1:a39, a_cosv1:a_cosv19) %>%
   alpha()
 
 #Ну, кайф. Альфа Кронбаха нормальная, даже хорошая. 
@@ -92,16 +91,16 @@ anket %>%
 
 #Kaiser-Meyer-Olkin (KMO)
 anket %>% 
-  select(a1:a_cosv18) %>% 
+  select(a1:a_cosv19) %>% 
   KMO() %>% 
   .[["MSAi"]] %>% sort()
 
-#Удаляем ещё один элемент
+#Удаляем ещё пару элементов
 
 anket %>% 
-  select(-a9) -> anket
+  select(-a5, -a26, -a21, -a22) -> anket
 
-anket %>% select(a1:a_cosv18) %>% colnames() -> vars_left
+anket %>% select(a1:a_cosv19) %>% colnames() -> vars_left
 
 vars_left
 
@@ -109,12 +108,15 @@ vars_left
 
 anket %>% 
   mutate(
-    a_sum_1 = rowSums(across(c(a1:a13, a_cosv2:a_cosv4)))) -> anket
+    a_sum_1 = rowSums(across(c(a1:a13, a_cosv1:a_cosv5))),
+    a_sum_2 = rowSums(across(c(a14:a27, a_cosv7:a_cosv10))),
+    a_sum_3 = rowSums(across(c(a28:a42, a_cosv11:a_cosv14))),
+    a_sum_4 = rowSums(across(c(a43:a56, a_cosv16:a_cosv19)))) -> anket
 
 #Альфа Кронбаха
 
 anket %>% 
-  select(a1:a39) %>%
+  select(a1:a_cosv19) %>%
   alpha()
 
 #Ну, кайф. Альфа Кронбаха нормальная, даже хорошая. 
@@ -122,7 +124,7 @@ anket %>%
 
 #Kaiser-Meyer-Olkin (KMO)
 anket %>% 
-  select(a1:a_cosv18) %>% 
+  select(a1:a_cosv19) %>% 
   KMO() %>% 
   .[["MSAi"]] %>% sort()
 
@@ -131,7 +133,7 @@ anket %>%
 #Давайте попробуем 1 фактор
 
 anket %>% 
-  select(a1:a_cosv18) %>%
+  select(a1:a_cosv19) %>%
   factanal(factors = 1) ->
   anket.fa1
 
@@ -139,13 +141,13 @@ print(anket.fa1$loadings, cutoff = 0.4)
 
 #Определим количество факторов
 anket %>% 
-  select(a1:a_cosv18) %>% 
+  select(a1:a_cosv19) %>% 
   fa.parallel(fa = "fa", fm = "ml")
 
 #Машина говорит, что 3-4. Но мы возьмём 4, потому что теория!!!
 
 anket %>% 
-  select(a1:a_cosv18) %>%
+  select(a1:a_cosv19) %>%
   factanal(factors = 4, rotation = 'oblimin') ->
   anket.fa4
 
@@ -158,10 +160,10 @@ vars_left
 
 #КФА
 
-mdl1 <- ('F1 =~ a8+a1+a2+a4+a6+a7+a11+a13+a_cosv3+a_cosv4
-          F2 =~ a16+a14+a15+a19+a20+a24+a25+a27+a_cosv7+a_cosv8++a_cosv9++a_cosv10
-          F3 =~ a42+a28+a29+a30+a32+a37+a38+a39+a41+a_cosv11+a_cosv12+a_cosv13+a_cosv14
-          F4 =~ a51+a43+a44+a46+a47+a50+a52+a55+a56+a_cosv16+a_cosv17+a_cosv18')
+mdl1 <- ('F1 =~ a8+a1+a2+a4+a6+a7+a9+a10+a11+a13+a_cosv1+a_cosv2+a_cosv3+a_cosv4+a_cosv5
+          F2 =~ a19+a14+a13+a14+a15+a16+a17+a20+a23+a24+a25+a27+a_cosv7+a_cosv8+a_cosv9+a_cosv10
+          F3 =~ a42+a28+a29+a30+a31+a32+a34+a35+a37+a38+a39+a41+a_cosv11+a_cosv12+a_cosv13+a_cosv14
+          F4 =~ a51+a43+a44+a45+a46+a47+a49+a50+a52+a53+a55+a56+a_cosv16+a_cosv17+a_cosv18+a_cosv19')
 
 model1 <- cfa(mdl1, data = anket)
 summary(model1)
@@ -174,8 +176,8 @@ fitmeasures(model1, c("chisq","cfi", "tli", "srmr", "rmsea"))
 
 anket %>% select(c(ID:a8, a7, a11, a_cosv3, a_cosv4, a6,
                  a16, a15, a19, a25, a_cosv7, a_cosv9,
-                 a42, a37, a38, a41, a39, a_cosv11,
-                 a51, a43, a50, a52, a55, a_cosv18:s_sum_1)) -> anket
+                 a42, a37, a38, a41, a39, a_cosv13,
+                 a51, a43, a52, a55, a_cosv16, a_cosv18, b1:s_sum_1)) -> anket
 
 #Бахаем напоследок всё то-же самое
 
@@ -185,8 +187,8 @@ anket %>%
   mutate(
     a_sum_1 = rowSums(across(c(a8, a7, a11, a_cosv3, a_cosv4, a6))),
     a_sum_2 = rowSums(across(c(a16, a15, a19, a25, a_cosv7, a_cosv9))),
-    a_sum_3 = rowSums(across(c(a42, a37, a38, a41, a39, a_cosv11))),
-    a_sum_4 = rowSums(across(c(a51, a43, a50, a52, a55, a_cosv18)))) -> anket
+    a_sum_3 = rowSums(across(c(a42, a37, a38, a41, a39, a_cosv13))),
+    a_sum_4 = rowSums(across(c(a51, a43, a52, a55, a_cosv16, a_cosv18)))) -> anket
 
 #Альфа Кронбаха
 
@@ -237,8 +239,8 @@ print(anket.fa4$loadings, cutoff = 0.4)
 
 mdl2 <- ('F1 =~ a8+a7+a11+a_cosv3+a_cosv4+a6
           F2 =~ a16+a15+a19+a25+a_cosv7+a_cosv9
-          F3 =~ a42+a37+a38+a41+a39+a_cosv11
-          F4 =~ a51+a43+a50+a52+a55+a_cosv18')
+          F3 =~ a42+a37+a38+a41+a39+a_cosv13
+          F4 =~ a51+a43+a52+a55+a_cosv16+a_cosv18')
 
 model2 <- cfa(mdl2, data = anket)
 lavInspect(model2, "cov.lv")
@@ -274,7 +276,7 @@ esem_loadings <- melt(esem_loadings, "item", variable.name = "latent")
 esem_loadings
 
 #Устанавливаем якоря
-anchors <- c(F1 = "a_cosv3", F2 = "a39", F3 = "a50")
+anchors <- c(F1 = "a_cosv18", F2 = "a6", F3 = "a25")
 
 #Задаём функцию для подсчёта ESEM модели
 make_esem_model <- function (loadings_dt, anchors){
@@ -314,33 +316,33 @@ lavaanPlot::lavaanPlot(model = esem_fit, coefs = TRUE,
                        edge_options = list(color ='grey'))
 
 #Ну, подумкаем. Волбшебная машина говорит, что фактора 3: 
-# 1) 8, 7, 11, cosv3, cosv4, 6, 16, 15, 51, 43
-# 2) 11, cosv7, 39, 55
-# 3) cosv9, 41, 50
+# 1) cosv9, 42, 41, cosv13, a51, a55, cosv16, cosv18
+# 2) 6, 7, 8, cosv3, cosv4, 43
+# 3) 25, 39
 # Обратимся теперь к магии теории, что это могут быть за факторы
-# 1) 8, 7, 11, cosv3, cosv4, 6, 16, 15, 51, 43 - про людей
-# 2) 11, cosv7, 39, 55 - про вовлечённость
-# 3) cosv9, 41, 50 - что-то про вещественную составляющую
+# 1) cosv9, 42, 41, cosv13, a51, a55, cosv16, cosv18 - с натяжкой что-то про страсть
+# 2) 6, 7, 8, cosv3, cosv4, 43 - про людей
+# 3) 25, 39 - что-то про настроение
 
 # Но в целом... Пациент скорее мёртв
 
 # А теперь возьмём то, что нам сказал E-SEM, и бахнем ещё одну CFA
-# Отсеим стандартный вклад ниже 0.7
+# Отсеим 2-4 вопроса для шкалы с самым большим вкладом
 
 #CFA - once again
 
-mdl3 <- ('F1 =~ a8+a7+a11+a_cosv3+a_cosv4+a6+a16+a15+a51+a43
-          F2 =~ a55+a11+a_cosv7+a39
-          F3 =~ a50+a_cosv9+a41')
+mdl3 <- ('F1 =~ a_cosv13+a51+a55+a_cosv18
+          F2 =~ a6+a7+a8+a_cosv3
+          F3 =~ a25+a39')
 
 model3 <- cfa(mdl3, data = anket)
 summary(model3)
 anket %>% 
-  select(c(ID:a6,a7,a_cosv3,a_cosv4,a8,a16,a15,a51,a43, a55, a_cosv7, a39, 
-           a50, a41,a_cosv9:s_sum_1)) -> anket
-mdl3 <- ('F1 =~ a8+a7+a_cosv3+a_cosv4+a6+a16+a15+a51+a43
-          F2 =~ a55+a_cosv7+a39
-          F3 =~ a50+a_cosv9+a41')
+  select(c(ID:a6, a_cosv13, a51, a55, 
+           a_cosv18, a7, a8, a_cosv3, a25, a39, b1:s_sum_1)) -> anket
+mdl3 <- ('F1 =~ a51+a_cosv13+a55+a_cosv18
+          F2 =~ a8+a6+a7+a_cosv3
+          F3 =~ a39+a25')
 
 model3 <- cfa(mdl3, data = anket)
 summary(model3)
@@ -354,6 +356,7 @@ fitmeasures(model2, c("chisq","cfi", "tli", "srmr", "rmsea"))
 fitmeasures(model3, c("chisq","cfi", "tli", "srmr", "rmsea"))
 
 # Окей, определённо модель с тремя латентными факторами, окей
+# Даже почти дотягивается до уровня приемлимости
 # Давайте её пофайнтюним и всё
 
 
@@ -362,32 +365,30 @@ fitmeasures(model3, c("chisq","cfi", "tli", "srmr", "rmsea"))
 semPaths(model3, 'std')
 modificationindices(model3) %>% filter(mi > 10) %>% arrange(-mi)
 
-#Lavaan предлагает учесть следующие взаимодействия:
-# Ковариация a_cosv3 и a16; a7 и a6; a_cosv3 и a50
-# Включить a15 и a6 в F3; a15 в F2
+# Lavaan предлагает учесть следующие взаимодействия:
+# Включить a39 и a25 в F1, F2
+# Включить a_cosv3 в F1 и а55 в F3
 
-# Я считаю оправданным учесть ковариацию a7 и a6; a_cosv3 и a50
-# Ковариацию a_cosv3 и a16 учитывать не будем
-# Включение пунктов в другие факторы считаю не оправданным
+# Я считаю оправданным включить a55 в F3
+# Включение других пунктов в другие факторы считаю не оправданным
 
 #Посмотрим после учёта
 
-mdl3.1 <- ('F1 =~ a8+a7+a_cosv3+a_cosv4+a6+a16+a15+a51+a43
-          F2 =~ a55+a_cosv7+a39
-          F3 =~ a50+a_cosv9+a41
-          a7 ~~  a6
-          a_cosv3 ~~ a50')
+mdl3.1 <- ('F1 =~ a51+a_cosv13+a55+a_cosv18
+          F2 =~ a8+a6+a7+a_cosv3
+          F3 =~ a39+a25+a55')
 
 model3.1 <- cfa(mdl3.1, data = anket)
 summary(model3.1)
 fitmeasures(model3.1, c("chisq","cfi", "tli", "srmr", "rmsea"))
+# В целом модель проходит по границе приемлимости
 
 #Строим график
 
 semPaths(model3.1, 'std')
-modificationindices(model3.1) %>% filter(mi > 10) %>% arrange(-mi)
 
-anket %>% select(a6:a_cosv9) %>% colnames() -> vars_left
+
+anket %>% select(a6:a_cosv18) %>% colnames() -> vars_left
 
 #Пересчитаем суммы
 #Посчитаем веса
@@ -406,9 +407,9 @@ for (x in c(14:28)) {
 #Сами суммы
 anket %>% 
   mutate(
-    a_sum_1 = rowSums(across(c(a8,a7,a_cosv3,a_cosv4,a6,a16,a15,a51,a43))),
-    a_sum_2 = rowSums(across(c(a55,a_cosv7,a39))),
-    a_sum_3 = rowSums(across(c(a50,a_cosv9,a41)))) -> anket
+    a_sum_1 = rowSums(across(c(a51,a_cosv13,a55,a_cosv18))),
+    a_sum_2 = rowSums(across(c(a8,a6,a7,a_cosv3))),
+    a_sum_3 = rowSums(across(c(a39,a25,a55)))) -> anket
 
 anket %>% select(-a_sum_4) -> anket
 
@@ -416,6 +417,12 @@ anket %>% select(-a_sum_4) -> anket
 
 anket %>% select(ID:d12, a_sum_1:s_sum_1) %>%
   write_csv(paste0(getwd(),'/2nd_survey/anket_an_1.csv'))
+
+# Теоретическая интерпретация
+
+# F1 (a51,a_cosv13,a55,a_cosv18) - смысл работы, значимость
+# F2 (a8,a6,a7,a_cosv3) - про людей
+# F3 (a39,a25,a55) - про настроение
 
 #Проверим методику "Сокращенная версия опросника проактивный копиг" -----
 #Альфа Кронбаха
