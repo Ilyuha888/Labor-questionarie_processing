86a96413a35e11b74bd099608ee5c66ba7455c1c
Ilyuha888 <nartov.nartov@gmail.com>
2024-06-01 10:15:32 +0300

    Final image fix

--- 2nd_survey/Anal_1.R
+++ 2nd_survey/Anal_1.R
@@ -16,6 +16,9 @@ library(tidySEM)
 #Импортируем почищенные данные
 anket <- read_csv(paste0(getwd(),'/2nd_survey/anket_clean.csv'))
 
+# Заменяем "a_cv" на "cv"
+colnames(anket)  <- gsub("a_cosv", "cv", colnames(anket))
+
 #Распределение по полу и возрасту возрасту
 anket %>% 
   group_by(d1) %>% 
@@ -40,20 +43,20 @@ View(kurtosis)
 
 
 anket %>% 
-  select(-a_cosv15, -a3) -> anket
+  select(-cv15, -a3) -> anket
 
 #Пересчитаем суммы
 
 anket %>% 
   mutate(
-    a_sum_1 = rowSums(across(c(a1:a13, a_cosv1:a_cosv5))),
-    a_sum_4 = rowSums(across(c(a43:a56, a_cosv16:a_cosv19)))) -> anket
+    a_sum_1 = rowSums(across(c(a1:a13, cv1:cv5))),
+    a_sum_4 = rowSums(across(c(a43:a56, cv16:cv19)))) -> anket
 
 #МОДЕЛЬ 1 -----
 #Альфа Кронбаха
 
 anket %>% 
-  select(a1:a56, a_cosv1:a_cosv19) %>%
+  select(a1:a56, cv1:cv19) %>%
   alpha()
 
 #Ну, кайф. Альфа Кронбаха нормальная, даже хорошая. Удаление ни одного пункта, 
@@ -61,7 +64,7 @@ anket %>%
 
 #Kaiser-Meyer-Olkin (KMO)
 anket %>% 
-  select(a1:a56, a_cosv1:a_cosv19) %>% 
+  select(a1:a56, cv1:cv19) %>% 
   KMO() %>% 
   .[["MSAi"]] %>% sort()
 
@@ -69,9 +72,9 @@ anket %>%
 #где показатель ниже 0.7
 
 anket %>% 
-  select(-a12, -a48, -a18, -a33, -a_cosv6, -a36, -a54, -a40) -> anket
+  select(-a12, -a48, -a18, -a33, -cv6, -a36, -a54, -a40) -> anket
 
-anket %>% select(a1:a_cosv19) %>% colnames() -> vars_left
+anket %>% select(a1:cv19) %>% colnames() -> vars_left
 
 vars_left
 
@@ -79,15 +82,15 @@ vars_left
 
 anket %>% 
   mutate(
-    a_sum_1 = rowSums(across(c(a1:a13, a_cosv1:a_cosv5))),
-    a_sum_2 = rowSums(across(c(a14:a27, a_cosv7:a_cosv10))),
-    a_sum_3 = rowSums(across(c(a28:a42, a_cosv11:a_cosv14))),
-    a_sum_4 = rowSums(across(c(a43:a56, a_cosv16:a_cosv19)))) -> anket
+    a_sum_1 = rowSums(across(c(a1:a13, cv1:cv5))),
+    a_sum_2 = rowSums(across(c(a14:a27, cv7:cv10))),
+    a_sum_3 = rowSums(across(c(a28:a42, cv11:cv14))),
+    a_sum_4 = rowSums(across(c(a43:a56, cv16:cv19)))) -> anket
 
 #Альфа Кронбаха
 
 anket %>% 
-  select(a1:a39, a_cosv1:a_cosv19) %>%
+  select(a1:a39, cv1:cv19) %>%
   alpha()
 
 #Ну, кайф. Альфа Кронбаха нормальная, даже хорошая. 
@@ -95,7 +98,7 @@ anket %>%
 
 #Kaiser-Meyer-Olkin (KMO)
 anket %>% 
-  select(a1:a_cosv19) %>% 
+  select(a1:cv19) %>% 
   KMO() %>% 
   .[["MSAi"]] %>% sort()
 
@@ -104,7 +107,7 @@ anket %>%
 anket %>% 
   select(-a5, -a26, -a21, -a22) -> anket
 
-anket %>% select(a1:a_cosv19) %>% colnames() -> vars_left
+anket %>% select(a1:cv19) %>% colnames() -> vars_left
 
 vars_left
 
@@ -112,15 +115,15 @@ vars_left
 
 anket %>% 
   mutate(
-    a_sum_1 = rowSums(across(c(a1:a13, a_cosv1:a_cosv5))),
-    a_sum_2 = rowSums(across(c(a14:a27, a_cosv7:a_cosv10))),
-    a_sum_3 = rowSums(across(c(a28:a42, a_cosv11:a_cosv14))),
-    a_sum_4 = rowSums(across(c(a43:a56, a_cosv16:a_cosv19)))) -> anket
+    a_sum_1 = rowSums(across(c(a1:a13, cv1:cv5))),
+    a_sum_2 = rowSums(across(c(a14:a27, cv7:cv10))),
+    a_sum_3 = rowSums(across(c(a28:a42, cv11:cv14))),
+    a_sum_4 = rowSums(across(c(a43:a56, cv16:cv19)))) -> anket
 
 #Альфа Кронбаха
 
 anket %>% 
-  select(a1:a_cosv19) %>%
+  select(a1:cv19) %>%
   alpha()
 
 #Ну, кайф. Альфа Кронбаха нормальная, даже хорошая. 
@@ -128,7 +131,7 @@ anket %>%
 
 #Kaiser-Meyer-Olkin (KMO)
 anket %>% 
-  select(a1:a_cosv19) %>% 
+  select(a1:cv19) %>% 
   KMO() %>% 
   .[["MSAi"]] %>% sort()
 
@@ -137,7 +140,7 @@ anket %>%
 #Давайте попробуем 1 фактор
 
 anket %>% 
-  select(a1:a_cosv19) %>%
+  select(a1:cv19) %>%
   factanal(factors = 1) ->
   anket.fa1
 
@@ -145,13 +148,13 @@ print(anket.fa1$loadings, cutoff = 0.4)
 
 #Определим количество факторов
 anket %>% 
-  select(a1:a_cosv19) %>% 
+  select(a1:cv19) %>% 
   fa.parallel(fa = "fa", fm = "ml")
 
 #Машина говорит, что 3-4. Но мы возьмём 4, потому что теория!!!
 
 anket %>% 
-  select(a1:a_cosv19) %>%
+  select(a1:cv19) %>%
   factanal(factors = 4, rotation = 'oblimin') ->
   anket.fa4
 
@@ -166,10 +169,10 @@ vars_left
 
 #КФА
 
-mdl1 <- ('F1 =~ a8+a1+a2+a4+a6+a7+a9+a10+a11+a13+a_cosv1+a_cosv2+a_cosv3+a_cosv4+a_cosv5
-          F2 =~ a19+a14+a13+a14+a15+a16+a17+a20+a23+a24+a25+a27+a_cosv7+a_cosv8+a_cosv9+a_cosv10
-          F3 =~ a42+a28+a29+a30+a31+a32+a34+a35+a37+a38+a39+a41+a_cosv11+a_cosv12+a_cosv13+a_cosv14
-          F4 =~ a51+a43+a44+a45+a46+a47+a49+a50+a52+a53+a55+a56+a_cosv16+a_cosv17+a_cosv18+a_cosv19')
+mdl1 <- ('F1 =~ a8+a1+a2+a4+a6+a7+a9+a10+a11+a13+cv1+cv2+cv3+cv4+cv5
+          F2 =~ a19+a14+a13+a14+a15+a16+a17+a20+a23+a24+a25+a27+cv7+cv8+cv9+cv10
+          F3 =~ a42+a28+a29+a30+a31+a32+a34+a35+a37+a38+a39+a41+cv11+cv12+cv13+cv14
+          F4 =~ a51+a43+a44+a45+a46+a47+a49+a50+a52+a53+a55+a56+cv16+cv17+cv18+cv19')
 
 model1 <- cfa(mdl1, data = anket)
 summary(model1)
@@ -188,23 +191,23 @@ semPaths(model1, "std", layout = "circle",
          curvePivot = TRUE,
          pastel = TRUE,
          shapeMan = 'circle',
-         node.width = 0.7,    # Увеличение ширины узлов
+         node.width = 0.9,    # Увеличение ширины узлов
          node.height = 0.7,   # Увеличение высоты узлов
          label.cex = 1.2,   # Масштабирование меток
          mar = c(1, 1, 1, 1),
          filetype = "png",
          filename = 'model1',
          width = 11.7,
-         height = 8.3)# Увеличение отступов
+         height = 8.3) # Увеличение отступов
 
 #МОДЕЛЬ 2 -----
 
 # Так, оставим везде по 5-6 самых нагурженных вопросов
 
-anket %>% select(c(ID:a8, a7, a11, a_cosv3, a_cosv4, a6,
-                 a16, a15, a19, a25, a_cosv7, a_cosv9,
-                 a42, a37, a38, a41, a39, a_cosv13,
-                 a51, a43, a52, a55, a_cosv16, a_cosv18, b1:s_sum_1)) -> anket
+anket %>% select(c(ID:a8, a7, a11, cv3, cv4, a6,
+                 a16, a15, a19, a25, cv7, cv9,
+                 a42, a37, a38, a41, a39, cv13,
+                 a51, a43, a52, a55, cv16, cv18, b1:s_sum_1)) -> anket
 
 #Бахаем напоследок всё то-же самое
 
@@ -212,15 +215,15 @@ anket %>% select(c(ID:a8, a7, a11, a_cosv3, a_cosv4, a6,
 
 anket %>% 
   mutate(
-    a_sum_1 = rowSums(across(c(a8, a7, a11, a_cosv3, a_cosv4, a6))),
-    a_sum_2 = rowSums(across(c(a16, a15, a19, a25, a_cosv7, a_cosv9))),
-    a_sum_3 = rowSums(across(c(a42, a37, a38, a41, a39, a_cosv13))),
-    a_sum_4 = rowSums(across(c(a51, a43, a52, a55, a_cosv16, a_cosv18)))) -> anket
+    a_sum_1 = rowSums(across(c(a8, a7, a11, cv3, cv4, a6))),
+    a_sum_2 = rowSums(across(c(a16, a15, a19, a25, cv7, cv9))),
+    a_sum_3 = rowSums(across(c(a42, a37, a38, a41, a39, cv13))),
+    a_sum_4 = rowSums(across(c(a51, a43, a52, a55, cv16, cv18)))) -> anket
 
 #Альфа Кронбаха
 
 anket %>% 
-  select(a6:a_cosv18) %>%
+  select(a6:cv18) %>%
   alpha()
 
 #Ну, кайф. Альфа Кронбаха нормальная, даже хорошая. 
@@ -228,7 +231,7 @@ anket %>%
 
 #Kaiser-Meyer-Olkin (KMO)
 anket %>% 
-  select(a6:a_cosv18) %>% 
+  select(a6:cv18) %>% 
   KMO() %>% 
   .[["MSAi"]] %>% sort()
 
@@ -236,16 +239,16 @@ anket %>%
 
 #ЭФА
 
-anket %>% select(a6:a_cosv18) %>% colnames() -> vars_left
+anket %>% select(a6:cv18) %>% colnames() -> vars_left
 
 #Определим количество факторов
 anket %>% 
-  select(a6:a_cosv18) %>% 
+  select(a6:cv18) %>% 
   fa.parallel(fa = "fa", fm = "ml")
 #Давайте попробуем 1 фактор
 
 anket %>% 
-  select(a6:a_cosv18) %>%
+  select(a6:cv18) %>%
   factanal(factors = 1) ->
   anket.fa1
 
@@ -254,7 +257,7 @@ print(anket.fa1$loadings, cutoff = 0.4)
 # Попробуем теоретическую модель
 
 anket %>% 
-  select(a6:a_cosv18) %>%
+  select(a6:cv18) %>%
   factanal(factors = 4, rotation = 'oblimin') ->
   anket.fa4
 
@@ -264,10 +267,10 @@ print(anket.fa4$loadings, cutoff = 0.4)
 
 #КФА
 
-mdl2 <- ('F1 =~ a8+a7+a11+a_cosv3+a_cosv4+a6
-          F2 =~ a16+a15+a19+a25+a_cosv7+a_cosv9
-          F3 =~ a42+a37+a38+a41+a39+a_cosv13
-          F4 =~ a51+a43+a52+a55+a_cosv16+a_cosv18')
+mdl2 <- ('F1 =~ a8+a7+a11+cv3+cv4+a6
+          F2 =~ a16+a15+a19+a25+cv7+cv9
+          F3 =~ a42+a37+a38+a41+a39+cv13
+          F4 =~ a51+a43+a52+a55+cv16+cv18')
 
 model2 <- cfa(mdl2, data = anket)
 lavInspect(model2, "cov.lv")
@@ -288,14 +291,14 @@ semPaths(model2, "std", layout = "circle",
          curvePivot = TRUE,
          pastel = TRUE,
          shapeMan = 'circle',
-         node.width = 0.7,    # Увеличение ширины узлов
+         node.width = 0.9,    # Увеличение ширины узлов
          node.height = 0.7,   # Увеличение высоты узлов
          label.cex = 1.2,   # Масштабирование меток
          mar = c(1, 1, 1, 1),
          filetype = "png",
          filename = 'model2',
          width = 11.7,
-         height = 8.3)# Увеличение отступов
+         height = 8.3) # Увеличение отступов
 
 modificationindices(model2) %>% filter(mi > 20) %>% arrange(-mi)
 
@@ -310,7 +313,7 @@ View(model2)
 
 #Первое вращение, выбираем якоря
 anket %>% 
-  select(a6:a_cosv18) %>%
+  select(a6:cv18) %>%
   factanal(factors = 3, rotation = 'geominQ', delta = .5) ->
   anket.esem.efa
 print(anket.esem.efa$loadings, cutoff = 0.4)
@@ -327,7 +330,7 @@ esem_loadings <- melt(esem_loadings, "item", variable.name = "latent")
 esem_loadings
 
 #Устанавливаем якоря
-anchors <- c(F1 = "a_cosv18", F2 = "a6", F3 = "a25")
+anchors <- c(F1 = "cv18", F2 = "a6", F3 = "a25")
 
 #Задаём функцию для подсчёта ESEM модели
 make_esem_model <- function (loadings_dt, anchors){
@@ -356,7 +359,7 @@ esem_model <- make_esem_model(esem_loadings, anchors)
 esem_model
 
 #Смотрим стандартные нагрузки, с фильтром больше или равно 0.7
-esem_fit <- cfa(esem_model, select(anket, a6:a_cosv18), std.lv=T)
+esem_fit <- cfa(esem_model, select(anket, a6:cv18), std.lv=T)
 summary(esem_fit, fit.measures = T, standardized = T) %>% 
   .[["pe"]] %>% 
   filter(std.lv >= 0.7)
@@ -367,12 +370,12 @@ lavaanPlot::lavaanPlot(model = esem_fit, coefs = TRUE,
                        edge_options = list(color ='grey'))
 
 #Ну, подумкаем. Волбшебная машина говорит, что фактора 3: 
-# 1) cosv9, 42, 41, cosv13, a51, a55, cosv16, cosv18
-# 2) 6, 7, 8, cosv3, cosv4, 43
+# 1) cv9, 42, 41, cv13, a51, a55, cv16, cv18
+# 2) 6, 7, 8, cv3, cv4, 43
 # 3) 25, 39
 # Обратимся теперь к магии теории, что это могут быть за факторы
-# 1) cosv9, 42, 41, cosv13, a51, a55, cosv16, cosv18 - с натяжкой что-то про страсть
-# 2) 6, 7, 8, cosv3, cosv4, 43 - про людей
+# 1) cv9, 42, 41, cv13, a51, a55, cv16, cv18 - с натяжкой что-то про страсть
+# 2) 6, 7, 8, cv3, cv4, 43 - про людей
 # 3) 25, 39 - что-то про настроение
 
 # Но в целом... Пациент скорее мёртв
@@ -382,17 +385,17 @@ lavaanPlot::lavaanPlot(model = esem_fit, coefs = TRUE,
 
 #CFA - once again
 
-mdl3 <- ('F1 =~ a_cosv13+a51+a55+a_cosv18
-          F2 =~ a6+a7+a8+a_cosv3
+mdl3 <- ('F1 =~ cv13+a51+a55+cv18
+          F2 =~ a6+a7+a8+cv3
           F3 =~ a25+a39')
 
 model3 <- cfa(mdl3, data = anket)
 summary(model3)
 anket %>% 
-  select(c(ID:a6, a_cosv13, a51, a55, 
-           a_cosv18, a7, a8, a_cosv3, a25, a39, b1:s_sum_1)) -> anket
-mdl3 <- ('F1 =~ a51+a_cosv13+a55+a_cosv18
-          F2 =~ a8+a6+a7+a_cosv3
+  select(c(ID:a6, cv13, a51, a55, 
+           cv18, a7, a8, cv3, a25, a39, b1:s_sum_1)) -> anket
+mdl3 <- ('F1 =~ a51+cv13+a55+cv18
+          F2 =~ a8+a6+a7+cv3
           F3 =~ a39+a25')
 
 model3 <- cfa(mdl3, data = anket)
@@ -418,15 +421,15 @@ modificationindices(model3) %>% filter(mi > 10) %>% arrange(-mi)
 
 # Lavaan предлагает учесть следующие взаимодействия:
 # Включить a39 и a25 в F1, F2
-# Включить a_cosv3 в F1 и а55 в F3
+# Включить cv3 в F1 и а55 в F3
 
 # Я считаю оправданным включить a55 в F3
 # Включение других пунктов в другие факторы считаю не оправданным
 
 #Посмотрим после учёта
 
-mdl3.1 <- ('F1 =~ a51+a_cosv13+a55+a_cosv18
-          F2 =~ a8+a6+a7+a_cosv3
+mdl3.1 <- ('F1 =~ a51+cv13+a55+cv18
+          F2 =~ a8+a6+a7+cv3
            F3 =~ a39+a25+a55')
 
 model3.1 <- cfa(mdl3.1, data = anket)
@@ -434,8 +437,7 @@ summary(model3.1)
 fitmeasures(model3.1, c("chisq","cfi", "tli", "srmr", "rmsea"))
 # В целом модель проходит по границе приемлимости
 
- #Строим график
-
+#Строим график
 semPaths(model3.1, "std", layout = "circle",
          style = "ram", 
          residuals = FALSE, 
@@ -446,17 +448,17 @@ semPaths(model3.1, "std", layout = "circle",
          curvePivot = TRUE,
          pastel = TRUE,
          shapeMan = 'circle',
-         node.width = 0.7,    # Увеличение ширины узлов
+         node.width = 0.9,    # Увеличение ширины узлов
          node.height = 0.7,   # Увеличение высоты узлов
          label.cex = 1.2,   # Масштабирование меток
          mar = c(1, 1, 1, 1),
          filetype = "png",
          filename = 'model3.1',
          width = 11.7,
-         height = 8.3)# Увеличение отступов
+         height = 8.3) # Увеличение отступов
 
 
-anket %>% select(a6:a_cosv18) %>% colnames() -> vars_left
+anket %>% select(a6:cv18) %>% colnames() -> vars_left
 
 #Пересчитаем суммы
 #Посчитаем веса
@@ -475,8 +477,8 @@ for (x in c(14:28)) {
 #Сами суммы
 anket %>% 
   mutate(
-    a_sum_1 = rowSums(across(c(a51,a_cosv13,a55,a_cosv18))),
-    a_sum_2 = rowSums(across(c(a8,a6,a7,a_cosv3))),
+    a_sum_1 = rowSums(across(c(a51,cv13,a55,cv18))),
+    a_sum_2 = rowSums(across(c(a8,a6,a7,cv3))),
     a_sum_3 = rowSums(across(c(a39,a25,a55)))) -> anket
 
 anket %>% select(-a_sum_4) -> anket
@@ -488,8 +490,8 @@ anket %>% select(ID:d12, a_sum_1:s_sum_1) %>%
 
 # Теоретическая интерпретация
 
-# F1 (a51,a_cosv13,a55,a_cosv18) - смысл работы, значимость
-# F2 (a8,a6,a7,a_cosv3) - про людей
+# F1 (a51,cv13,a55,cv18) - смысл работы, значимость
+# F2 (a8,a6,a7,cv3) - про людей
 # F3 (a39,a25,a55) - про настроение
 
 #Проверим методику "Сокращенная версия опросника проактивный копиг" -----
